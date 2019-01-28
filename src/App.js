d6b952e045e1ef77d298f5ba3975b276f76ccb60
DESKTOP-DD\Dimitar <dimitar@megantic.net.au>
2019-01-28 13:37:02 +0100

    CSS style added to logo.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React, { Component } from 'react';
-// import logo from './logo.svg';
-import './App.css';
+import logo from './logo.svg';
+import classes from './App.css';
 import Layout from './components/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        {/* <header className="App-header">
-          <img src={logo} className="logo" alt="logo" />
-        </header> */}
+      <div className={classes.App}>
+        <header className={classes.AppHeader}>
+          <img src={logo} className={classes.AppLogo} alt="logo" />
+        </header>
         <Layout>
           <BurgerBuilder />
         </Layout>
