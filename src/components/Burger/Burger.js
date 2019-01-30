86d465483ca28b7b74f56a8ce8eaf83ab9a86641
DESKTOP-DD\Dimitar <dimitar@megantic.net.au>
2019-01-30 11:23:44 +0100

    Connecting State to Build Controls.

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,14 +3,18 @@ import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
-    let passedIngredients = Object.keys(props.ingredients).map(igKey => {
+    let passedIngredients = Object.keys(props.ingredients)
+    .map(igKey => {
        return [...Array(props.ingredients[igKey])].map((_, i) => {
-           return <BurgerIngredient key={igKey + i} type={igKey} />;
+           return <BurgerIngredient key={igKey + i} type={igKey} />
        });
     })
     .reduce((arr, el) => {
         return arr.concat(el)
     }, []);
+
+    console.log(passedIngredients);
+    
 
     if(passedIngredients.length === 0) {
         passedIngredients = <p>Please add some ingredients!</p>
