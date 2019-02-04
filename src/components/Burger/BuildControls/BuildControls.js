a3d1e1286ce035b19444906c348aed01fa80dda1
DESKTOP-DD\Dimitar <dimitar@megantic.net.au>
2019-02-04 11:32:33 +0100

    Adding the Order Button.

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -1,27 +1,35 @@
-import React from 'react';
-import classes from './BuildControls.css';
-import BuildControl from './BuildControl/BuildControl';
+import React from "react";
+import classes from "./BuildControls.css";
+import BuildControl from "./BuildControl/BuildControl";
 
 const controls = [
-    { label: 'Salad', type: 'salad'},
-    { label: 'Bacon', type: 'bacon'},
-    { label: 'Cheese', type: 'cheese'},
-    { label: 'Meat', type: 'meat'}
-]
+  { label: "Salad", type: "salad" },
+  { label: "Bacon", type: "bacon" },
+  { label: "Cheese", type: "cheese" },
+  { label: "Meat", type: "meat" }
+];
 
-const buildControls = (props) => (
-    <div className={classes.BuildControls}>
-        <p style={{fontSize: '35px'}}>Current Price: <strong style={{textDecoration: 'underline'}}>{props.price.toFixed(2)}</strong></p>
-        {controls.map(ctrl => (
-            <BuildControl 
-                key={ctrl.label} 
-                label={ctrl.label} 
-                added={() => props.ingredientAdded(ctrl.type)}
-                deducted={() => props.ingredientDeducted(ctrl.type)}
-                disabled={props.disabled[ctrl.type]}
-            />
-        ))}
-    </div>
+const buildControls = props => (
+  <div className={classes.BuildControls}>
+    <p style={{ fontSize: "35px" }}>
+      Current Price:{" "}
+      <strong style={{ textDecoration: "underline" }}>
+        {props.price.toFixed(2)}
+      </strong>
+    </p>
+    {controls.map(ctrl => (
+      <BuildControl
+        key={ctrl.label}
+        label={ctrl.label}
+        added={() => props.ingredientAdded(ctrl.type)}
+        deducted={() => props.ingredientDeducted(ctrl.type)}
+        disabled={props.disabled[ctrl.type]}
+      />
+    ))}
+    <button
+      className={classes.OrderButton}
+      disabled={!props.purchaseable}>Order Now</button>
+  </div>
 );
 
 export default buildControls;
