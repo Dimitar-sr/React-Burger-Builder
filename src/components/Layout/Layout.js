bf4f4ab998cc326d09fbabc48e87f018f00f5ac3
DESKTOP-DD\Dimitar <dimitar@megantic.net.au>
2019-02-06 13:04:34 +0100

    Reusing the Backdrop, Adding a Sidedrawer Toggle Button, Adding a Hamburger Icon.

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,20 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Outside from '../../hoc/Outside';
 import classes from './Layout.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-const layout = (props) => (
-    <Outside>
-        <Toolbar />
-        <SideDrawer />
-        <div>Backdrop</div>
-        <main className={classes.Content}>
-            {props.children}
-        </main>
-    </Outside>
-);
+class Layout extends Component {
+    state = {
+        showSideDrawer: false
+    }
 
+    sideDrawerClosedHandler = () => {
+        this.setState({ showSideDrawer: false })
+    }
 
-export default layout;
+    sideDrawerToggleHandler = () => {
+        this.setState((prevState) => {
+            return { showSideDrawer: !prevState.showSideDrawer };
+        });
+    }
+
+    render() {
+        return (
+            <Outside>
+                <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler} />
+                <SideDrawer
+                    open={this.state.showSideDrawer}
+                    closed={this.sideDrawerClosedHandler} />
+                <div>Backdrop</div>
+                <main className={classes.Content}>
+                    {this.props.children}
+                </main>
+            </Outside>
+        )
+    }
+}
+
+export default Layout;
 
--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -2,16 +2,26 @@ import React from 'react';
 import LogoImg from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
 import classes from './SideDrawer.css';
+import Backdrop from '../../UI/Backdrop/Backdrop';
+import Outside from '../../../hoc/Outside';
 
 const sideDrawer = (props) => {
-    // ... 
+    let attachedClasses = [classes.SideDrawer, classes.Close];
+    if (props.open) {
+        attachedClasses = [classes.SideDrawer, classes.Open];
+    }
+
     return (
-        <div className={classes.SideDrawer}>
-            <LogoImg height="11%" />
-            <nav>
-                <NavigationItems />
-            </nav>
-        </div>
+        <Outside>
+            <Backdrop show={props.open} clicked={props.closed} />
+            <div className={attachedClasses.join(' ')}>
+                <LogoImg height="11%" />
+                <nav>
+                    <NavigationItems />
+                </nav>
+            </div>
+        </Outside>
+
     );
 }
 
