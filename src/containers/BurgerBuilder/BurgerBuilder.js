af461e4e0a6fc66d23c248135a7452d324b00625
DESKTOP-DD\Dimitar <dimitar@megantic.net.au>
2019-03-08 12:04:30 +0100

    Navigating to the Checkout Page.

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -55,28 +55,29 @@ class BurgerBuilder extends Component {
 
   purchaseContinueHandler = () => {
     //alert('You continue');
-    this.setState({ loading: true });
-    const order = {
-      ingredients: this.state.ingredients,
-      price: this.state.totalPrice,
-      customer: {
-        name: 'Dimitar',
-        address: {
-          street: 'Ulica',
-          zipCode: '2400',
-          country: 'Macedonia'
-        },
-        email: 'test@mail.com'
-      },
-      deliveryMethod: 'fastest'
-    }
-    axios.post('/orders.json', order)
-      .then(response => {
-        this.setState({ loading: false, purchasing: false })
-      })
-      .catch(error => {
-        this.setState({ loading: false, purchasing: false })
-      });
+    // this.setState({ loading: true });
+    // const order = {
+    //   ingredients: this.state.ingredients,
+    //   price: this.state.totalPrice,
+    //   customer: {
+    //     name: 'Dimitar',
+    //     address: {
+    //       street: 'Ulica',
+    //       zipCode: '2400',
+    //       country: 'Macedonia'
+    //     },
+    //     email: 'test@mail.com'
+    //   },
+    //   deliveryMethod: 'fastest'
+    // }
+    // axios.post('/orders.json', order)
+    //   .then(response => {
+    //     this.setState({ loading: false, purchasing: false })
+    //   })
+    //   .catch(error => {
+    //     this.setState({ loading: false, purchasing: false })
+    //   });
+    this.props.history.push('/checkout');
   };
 
   addIngredientHandler = type => {
